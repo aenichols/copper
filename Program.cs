28b6348a848c2df495752a2a42616aa1d38fb345
Anthony Nichols <anthony.nichols@bngholdingsinc.com>
2022-03-28 13:43:18 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace copper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
